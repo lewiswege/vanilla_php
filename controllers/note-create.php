bd260e2686125914c3d6d55639ecedeb0e288d2d
lewiswege <wegelewis7@gmail.com>
2025-05-28 06:13:13 +0300

    Sanitising user input

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,8 +1,17 @@
 <?php
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    die_dump($_POST);
-}
+$config = require('config.php');
+$db = new Database($config['Database']);
 
 $heading = 'Create Note';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
+        'body' => $_POST['body'],
+        'user_id' => 1
+    ]);
+}
+
+
+
 require 'views/note-create.view.php';
