4e07877580f64bdca1356151f9cb7e201ac9f31c
lewiswege <wegelewis7@gmail.com>
2025-06-17 06:19:41 +0300

    Added password hashing functionality

--- controllers/registration/store.php
+++ controllers/registration/store.php
@@ -34,7 +34,7 @@ if ($user) {
 } else {
     $db->query('insert into users(email, password) values(:email,:password', [
         'email' => $email,
-        'password' => $password
+        'password' => password_hash($password, PASSWORD_BCRYPT)
     ]);
 
     $_SESSION['user'] = [
@@ -44,4 +44,3 @@ if ($user) {
     header('location: /');
     exit();
 }
-
